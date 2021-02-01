3c96d083b011d4d1e28bffafb46b6c4c1fa934c5
Reyes, Michael <mradianlogs@gmail.com>
2021-01-31 23:47:02 -0600

    Fleshed out a decent Feature data object which should represent most character feature cases. Worked on how setting up character features will work.

--- src/com/company/Character/Features.java
+++ src/com/company/Character/Features.java
@@ -2,48 +2,100 @@ package com.company.Character;
 
 import com.company.DataValues.Feature;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Features {
-    private List<Feature> classFeatures;
-    private List<Feature> raceFeatures;
-    private List<Feature> backgroundFeatures;
-    private List<Feature> customFeatures;
+    private Map<String, Feature> classFeatures;
+    private Map<String, Feature> raceFeatures;
+    private Map<String, Feature> backgroundFeatures;
+    private Map<String, Feature> customFeatures;
 
     public Features()
     {
-        classFeatures = new ArrayList<>();
-        raceFeatures = new ArrayList<>();
-        backgroundFeatures = new ArrayList<>();
-        customFeatures = new ArrayList<>();
+        classFeatures = new HashMap<>();
+        raceFeatures = new HashMap<>();
+        backgroundFeatures = new HashMap<>();
+        customFeatures = new HashMap<>();
     }
 
-    public List<Feature> getClassFeatures() {
+    public void setupFeatures()
+    {
+        classFeatures.clear();
+        raceFeatures.clear();
+        backgroundFeatures.clear();
+        customFeatures.clear();
+    }
+
+    public void addClassFeature(Feature newFeature)
+    {
+        if(classFeatures.get(newFeature.getName()) != null)
+            classFeatures.put(newFeature.getName(), newFeature);
+    }
+    public void removeClassFeature(String featureName)
+    {
+        if(classFeatures.get(featureName) != null)
+            classFeatures.remove(featureName);
+    }
+
+    public void addRaceFeature(Feature newFeature)
+    {
+        if(raceFeatures.get(newFeature.getName()) != null)
+            raceFeatures.put(newFeature.getName(), newFeature);
+    }
+    public void removeRaceFeature(String featureName)
+    {
+        if(raceFeatures.get(featureName) != null)
+            raceFeatures.remove(featureName);
+    }
+
+    public void addBackgroundFeature(Feature newFeature)
+    {
+        if(backgroundFeatures.get(newFeature.getName()) != null)
+            backgroundFeatures.put(newFeature.getName(), newFeature);
+    }
+    public void removeBackgroundFeature(String featureName)
+    {
+        if(backgroundFeatures.get(featureName) != null)
+            backgroundFeatures.remove(featureName);
+    }
+
+    public void addCustomFeature(Feature newFeature)
+    {
+        if(customFeatures.get(newFeature.getName()) != null)
+            customFeatures.put(newFeature.getName(), newFeature);
+    }
+    public void removeCustomFeature(String featureName)
+    {
+        if(customFeatures.get(featureName) != null)
+            customFeatures.remove(featureName);
+    }
+
+    public Map<String, Feature> getClassFeatures() {
         return classFeatures;
     }
-    public void setClassFeatures(List<Feature> classFeatures) {
+    public void setClassFeatures(Map<String, Feature> classFeatures) {
         this.classFeatures = classFeatures;
     }
 
-    public List<Feature> getRaceFeatures() {
+    public Map<String, Feature> getRaceFeatures() {
         return raceFeatures;
     }
-    public void setRaceFeatures(List<Feature> raceFeatures) {
+    public void setRaceFeatures(Map<String, Feature> raceFeatures) {
         this.raceFeatures = raceFeatures;
     }
 
-    public List<Feature> getBackgroundFeatures() {
+    public Map<String, Feature> getBackgroundFeatures() {
         return backgroundFeatures;
     }
-    public void setBackgroundFeatures(List<Feature> backgroundFeatures) {
+    public void setBackgroundFeatures(Map<String, Feature> backgroundFeatures) {
         this.backgroundFeatures = backgroundFeatures;
     }
 
-    public List<Feature> getCustomFeatures() {
+    public Map<String, Feature> getCustomFeatures() {
         return customFeatures;
     }
-    public void setCustomFeatures(List<Feature> customFeatures) {
+    public void setCustomFeatures(Map<String, Feature> customFeatures) {
         this.customFeatures = customFeatures;
     }
 }
--- src/com/company/DataValues/Feature.java
+++ src/com/company/DataValues/Feature.java
@@ -1,7 +1,135 @@
 package com.company.DataValues;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Feature {
     private String name;
     private String description;
     private String origin; //Ex: Race, Background, Etc.
+    private int levelCap; // -1 if not applicable.
+
+    private boolean hasLimitedUsages; //Used "once per short/long rest" "3 times per short/long rest"
+    private int maxNumUsages;
+    private int numUsagesLeft;
+    private String maxUsageDuration; //Long rest, short rest, etc.
+
+    private boolean hasSubFeatures;
+    private Map<String, Feature> subFeatures;
+
+    public Feature(String name, String description, String origin, int levelCap, boolean hasLimitedUsages,
+                   int maxNumUsages, int numUsagesLeft, String maxUsageDuration, boolean hasSubFeatures) {
+        this.name = name;
+        this.description = description;
+        this.origin = origin;
+        this.levelCap = levelCap;
+        this.hasLimitedUsages = hasLimitedUsages;
+        this.maxNumUsages = maxNumUsages;
+        this.numUsagesLeft = numUsagesLeft;
+        this.maxUsageDuration = maxUsageDuration;
+        this.hasSubFeatures = hasSubFeatures;
+
+        if(hasSubFeatures)
+            this.subFeatures = new HashMap<>();
+    }
+
+    public void addSubFeature(Feature newSubFeature)
+    {
+        if(subFeatures == null)
+        {
+            subFeatures = new HashMap<>();
+            hasSubFeatures = true;
+            subFeatures.put(newSubFeature.getName(), newSubFeature);
+        }
+        else if(!subFeatures.containsKey(newSubFeature.getName()))
+        {
+            subFeatures.put(newSubFeature.getName(), newSubFeature);
+        }
+    }
+    public void removeSubFeature(String featureName)
+    {
+        if(subFeatures != null && subFeatures.containsKey(featureName))
+        {
+            subFeatures.remove(featureName);
+
+            int subFeaturesSizeLeftover = subFeatures.size();
+            if(subFeaturesSizeLeftover <= 0)
+            {
+                subFeatures = null;
+                hasSubFeatures = false;
+            }
+        }
+    }
+
+    //Getters and Setters.
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public int getLevelCap() {
+        return levelCap;
+    }
+    public void setLevelCap(int levelCap) {
+        this.levelCap = levelCap;
+    }
+
+    public boolean hasLimitedUsages() {
+        return hasLimitedUsages;
+    }
+    public void setHasLimitedUsages(boolean hasLimitedUsages) {
+        this.hasLimitedUsages = hasLimitedUsages;
+    }
+
+    public int getMaxNumUsages() {
+        return maxNumUsages;
+    }
+    public void setMaxNumUsages(int maxNumUsages) {
+        this.maxNumUsages = maxNumUsages;
+    }
+
+    public int getNumUsagesLeft() {
+        return numUsagesLeft;
+    }
+    public void setNumUsagesLeft(int numUsagesLeft) {
+        this.numUsagesLeft = numUsagesLeft;
+    }
+
+    public String getMaxUsageDuration() {
+        return maxUsageDuration;
+    }
+    public void setMaxUsageDuration(String maxUsageDuration) {
+        this.maxUsageDuration = maxUsageDuration;
+    }
+
+    public boolean hasSubFeatures() {
+        return hasSubFeatures;
+    }
+    public void setHasSubFeatures(boolean hasSubFeatures) {
+        this.hasSubFeatures = hasSubFeatures;
+    }
+
+    public Map<String, Feature> getSubFeatures() {
+        return subFeatures;
+    }
+    public void setSubFeatures(Map<String, Feature> subFeatures) {
+        this.subFeatures = subFeatures;
+    }
+
 }
